b7af9fbc35face5f9b51b194b75a59103e4dd7f4
tangok123 <15574658957@163.com>
2025-09-16 06:55:10 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,10 +5,7 @@ import os
 
 # 目标URL列表
 urls = [
-    'https://ip.164746.xyz', 
-     
-   
-    'https://www.wetest.vip/page/cloudflare/address_v4.html'
+    'https://api.uouin.com/cloudflare.html'
 ]
 
 # 正则表达式用于匹配IP地址
