8dbd66156fa4b67699e1cd505df5c750342e2391
tangok123 <15574658957@163.com>
2025-09-15 22:12:01 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,7 @@ import os
 # 目标URL列表
 urls = [
     'https://ip.164746.xyz', 
-    'https://cf.090227.xyz', 
+     
     'https://stock.hostmonit.com/CloudFlareYes',
     'https://www.wetest.vip/page/cloudflare/address_v4.html'
 ]
