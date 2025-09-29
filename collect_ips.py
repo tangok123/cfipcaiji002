bbc9e8bd133e2fd53c7e7866e1c2e5c654620bb6
tangok123 <15574658957@163.com>
2025-09-29 12:20:48 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,7 +6,7 @@ import os
 # 目标URL列表
 urls = [
     'https://www.wetest.vip/page/cloudflare/address_v4.html', 
-        'https://ipdb.030101.xyz/'
+        'https://ipdb.030101.xyz/bestcfv4/'
 ]
 
 # 正则表达式用于匹配IP地址
