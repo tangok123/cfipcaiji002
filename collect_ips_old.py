7be6b82039c8775802e180b469fc5329503ec0e9
tangok123 <15574658957@163.com>
2025-09-29 12:11:43 +0800

    Update collect_ips_old.py

--- collect_ips_old.py
+++ collect_ips_old.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://www.wetest.vip/page/cloudflare/address_v4.html', 
-        'https://ip.164746.xyz'
+        'https://ipdb.030101.xyz/bestcfv4'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -27,7 +27,7 @@ with open('ip.txt', 'w') as file:
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
             elements = soup.find_all('tr')
-        elif url == 'https://ip.164746.xyz':
+        elif url == 'https://ipdb.030101.xyz/bestcfv4':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
